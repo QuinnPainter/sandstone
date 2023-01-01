e0b8c3d591fac578132fe66678f33df87da46895
Quinn Painter <quinn@geekymonkey.com>
2023-01-01 01:17:28 +0000

    basic UI setup with docking

--- editor/src/main.rs
+++ editor/src/main.rs
@@ -1,112 +1,48 @@
-use std::time::Instant;
+mod ui;
 
-use glow::HasContext;
-use glutin::{event_loop::EventLoop, WindowedContext};
-use imgui_winit_support::WinitPlatform;
-
-const TITLE: &str = "Hello, imgui-rs!";
-
-type Window = WindowedContext<glutin::PossiblyCurrent>;
+use imgui::{WindowFlags, Condition};
 
 fn main() {
-    // Common setup for creating a winit window and imgui context, not specifc
-    // to this renderer at all except that glutin is used to create the window
-    // since it will give us access to a GL context
-    let (event_loop, window) = create_window();
-    let (mut winit_platform, mut imgui_context) = imgui_init(&window);
-
-    // OpenGL context from glow
-    let gl = glow_context(&window);
-
-    // OpenGL renderer from this crate
-    let mut ig_renderer = imgui_glow_renderer::AutoRenderer::initialize(gl, &mut imgui_context)
-        .expect("failed to create renderer");
-
-    let mut last_frame = Instant::now();
-
-    // Standard winit event loop
-    event_loop.run(move |event, _, control_flow| {
-        match event {
-            glutin::event::Event::NewEvents(_) => {
-                let now = Instant::now();
-                imgui_context
-                    .io_mut()
-                    .update_delta_time(now.duration_since(last_frame));
-                last_frame = now;
-            }
-            glutin::event::Event::MainEventsCleared => {
-                winit_platform
-                    .prepare_frame(imgui_context.io_mut(), window.window())
-                    .unwrap();
-                window.window().request_redraw();
-            }
-            glutin::event::Event::RedrawRequested(_) => {
-                // The renderer assumes you'll be clearing the buffer yourself
-                unsafe { ig_renderer.gl_context().clear(glow::COLOR_BUFFER_BIT) };
-
-                let ui = imgui_context.frame();
-                ui.show_demo_window(&mut true);
-
-                winit_platform.prepare_render(ui, window.window());
-                let draw_data = imgui_context.render();
-
-                // This is the only extra render step to add
-                ig_renderer
-                    .render(draw_data)
-                    .expect("error rendering imgui");
-
-                window.swap_buffers().unwrap();
-            }
-            glutin::event::Event::WindowEvent {
-                event: glutin::event::WindowEvent::CloseRequested,
-                ..
-            } => {
-                *control_flow = glutin::event_loop::ControlFlow::Exit;
-            }
-            event => {
-                winit_platform.handle_event(imgui_context.io_mut(), window.window(), &event);
-            }
+    ui::mainloop(move |ui| {
+        //ui.show_demo_window(&mut true);
+        // seems that imgui-rs has no abstraction for this yet, so we must use the raw binding
+        unsafe {
+            let view = imgui::sys::igGetMainViewport();
+            let win_class = imgui::sys::ImGuiWindowClass_ImGuiWindowClass();
+            imgui::sys::igDockSpaceOverViewport(view, 0, win_class);
         }
+        ui.main_menu_bar(|| {
+            let _ = ui.menu_item("Stuff");
+            let _ = ui.menu_item("things");
+        });
+
+        /*let (view_pos, view_size) = unsafe {
+            let view = imgui_sys::igGetMainViewport();
+            ((*view).WorkPos, (*view).WorkSize)
+        };
+        let style_no_rounding = ui.push_style_var(imgui::StyleVar::WindowRounding(0.0));
+        let style_no_border = ui.push_style_var(imgui::StyleVar::WindowBorderSize(0.0));
+        ui.window("Main")
+            .flags(WindowFlags::NO_TITLE_BAR | WindowFlags::NO_COLLAPSE | WindowFlags::NO_RESIZE |
+                WindowFlags::NO_BRING_TO_FRONT_ON_FOCUS | WindowFlags::NO_NAV_FOCUS |
+                WindowFlags::MENU_BAR | WindowFlags::NO_DOCKING)
+            .size([view_size.x, view_size.y], Condition::Always)
+            .position([view_pos.x, view_pos.y], Condition::Always)
+            .build(|| {
+                unsafe {
+                }
+            });
+        style_no_border.pop();
+        style_no_rounding.pop();*/
+        ui.window("Inspector")
+            .size([100.0, 50.0], Condition::FirstUseEver)
+            .build(|| {
+                ui.text("Gday\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\neef");
+            });
+        ui.window("Hierarchy")
+            .size([100.0, 50.0], Condition::FirstUseEver)
+            .build(|| {
+                ui.text("mate");
+            });
     });
-}
-
-fn create_window() -> (EventLoop<()>, Window) {
-    let event_loop = glutin::event_loop::EventLoop::new();
-    let window = glutin::window::WindowBuilder::new()
-        .with_title(TITLE)
-        .with_inner_size(glutin::dpi::LogicalSize::new(1024, 768));
-    let window = glutin::ContextBuilder::new()
-        .with_vsync(true)
-        .build_windowed(window, &event_loop)
-        .expect("could not create window");
-    let window = unsafe {
-        window
-            .make_current()
-            .expect("could not make window context current")
-    };
-    (event_loop, window)
-}
-
-fn glow_context(window: &Window) -> glow::Context {
-    unsafe { glow::Context::from_loader_function(|s| window.get_proc_address(s).cast()) }
-}
-
-fn imgui_init(window: &Window) -> (WinitPlatform, imgui::Context) {
-    let mut imgui_context = imgui::Context::create();
-    imgui_context.set_ini_filename(None);
-
-    let mut winit_platform = WinitPlatform::init(&mut imgui_context);
-    winit_platform.attach_window(
-        imgui_context.io_mut(),
-        window.window(),
-        imgui_winit_support::HiDpiMode::Rounded,
-    );
-
-    imgui_context
-        .fonts()
-        .add_font(&[imgui::FontSource::DefaultFontData { config: None }]);
-
-    imgui_context.io_mut().font_global_scale = (1.0 / winit_platform.hidpi_factor()) as f32;
-
-    (winit_platform, imgui_context)
 }
