da33a65d8f0e8ef71a16069f8a95019b36b3585a
Quinn Painter <quinn@geekymonkey.com>
2023-01-25 19:48:11 +0000

    Start on common crate used for data serialisation

---
+++ common/src/lib.rs
@@ -0,0 +1,33 @@
+#![no_std]
+extern crate alloc;
+use alloc::{string::String, vec::Vec};
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct SavedTransform {
+    pub x: u32,
+    pub y: u32
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct SavedNode {
+    pub child_index: u32,
+    pub sibling_index: u32,
+    pub name: String,
+    pub transform: SavedTransform,
+    pub script_type_id: Option<u32>,
+    pub enabled: bool
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct SavedNodeGraph {
+    pub nodes: Vec<SavedNode>
+}
+
+pub fn do_serialize(h: SavedNodeGraph) -> Vec<u8> {
+    postcard::to_allocvec(&h).unwrap()
+}
+
+pub fn do_deserialize(h: &[u8]) -> SavedNodeGraph {
+    postcard::from_bytes(h).unwrap()
+}
