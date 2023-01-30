f83ceb53783fa171ca8138bbb2a4e363ff692763
Quinn Painter <quinn@geekymonkey.com>
2023-01-30 15:01:13 +0000

    Change draw_hierarchy_node to method

--- editor/src/hierarchy.rs
+++ editor/src/hierarchy.rs
@@ -66,54 +66,53 @@ impl Hierarchy {
                     if ui.selectable("Add Stuff") {}
                     if ui.selectable("Things") {}
                 }
-                draw_hierarchy_node(ui, &self.graph, 0, &mut self.selected_node_idx);
+                self.draw_hierarchy_node(ui, 0);
             });
     }
-}
 
-// this function is quite a mess. wonder if it can be cleaned up and made into a method of Hierarchy
-fn draw_hierarchy_node(ui: &Ui, graph: &Vec<SavedNode>, node_idx: usize, selected_node_idx: &mut Option<NonZeroUsize>) {
-    if let Some(node) = graph.get(node_idx) {
-        let mut tree_node_token: Option<imgui::TreeNodeToken> = None;
-        // Root node is not drawn
-        if node_idx != 0 {
-            let mut flags = TreeNodeFlags::empty();
-            flags.set(TreeNodeFlags::OPEN_ON_ARROW, true);
-            flags.set(TreeNodeFlags::LEAF, node.child_index.is_none());
-            // could change this to is_some_and if that gets stablised
-            // flags.set(TreeNodeFlags::SELECTED, selected_node_idx.is_some_and(|x| usize::from(x) == node_idx));
-            flags.set(TreeNodeFlags::SELECTED, matches!(selected_node_idx, Some(x) if usize::from(*x) == node_idx));
-
-            tree_node_token = ui.tree_node_config(format!("{}##TreeNode{}", node.name, node_idx).as_str()).flags(flags).push();
-            if ui.is_item_clicked() {
-                *selected_node_idx = Some(NonZeroUsize::new(node_idx).unwrap());
-            }
-            if let Some(tooltip) = ui.drag_drop_source_config("HierarchyDragDrop").begin_payload(node_idx) {
-                // The tooltip displayed when dragging. This allocates every frame - could be improved?
-                ui.text(node.name.clone());
-                tooltip.end();
-            }
-            if let Some(target) = ui.drag_drop_target() {
-                let drag_drop_flags = imgui::DragDropFlags::empty();
-                if let Some(Ok(payload)) = target.accept_payload::<usize, _>("HierarchyDragDrop", drag_drop_flags) {
-                    //set node_idx parent to payload.data
+    fn draw_hierarchy_node(&mut self, ui: &Ui, node_idx: usize) {
+        if let Some(node) = self.graph.get(node_idx) {
+            let mut tree_node_token: Option<imgui::TreeNodeToken> = None;
+            // Root node is not drawn
+            if node_idx != 0 {
+                let mut flags = TreeNodeFlags::empty();
+                flags.set(TreeNodeFlags::OPEN_ON_ARROW, true);
+                flags.set(TreeNodeFlags::LEAF, node.child_index.is_none());
+                // could change this to is_some_and if that gets stablised
+                // flags.set(TreeNodeFlags::SELECTED, selected_node_idx.is_some_and(|x| usize::from(x) == node_idx));
+                flags.set(TreeNodeFlags::SELECTED, matches!(self.selected_node_idx, Some(x) if usize::from(x) == node_idx));
+    
+                tree_node_token = ui.tree_node_config(format!("{}##TreeNode{}", node.name, node_idx).as_str()).flags(flags).push();
+                if ui.is_item_clicked() {
+                    self.selected_node_idx = Some(NonZeroUsize::new(node_idx).unwrap());
                 }
-                target.pop();
+                if let Some(tooltip) = ui.drag_drop_source_config("HierarchyDragDrop").begin_payload(node_idx) {
+                    // The tooltip displayed when dragging. This allocates every frame - could be improved?
+                    ui.text(node.name.clone());
+                    tooltip.end();
+                }
+                if let Some(target) = ui.drag_drop_target() {
+                    let drag_drop_flags = imgui::DragDropFlags::empty();
+                    if let Some(Ok(payload)) = target.accept_payload::<usize, _>("HierarchyDragDrop", drag_drop_flags) {
+                        //set node_idx parent to payload.data
+                    }
+                    target.pop();
+                }
             }
-        }
-        if node_idx == 0 || tree_node_token.is_some() {
-            // Draw child nodes recursively
-            if let Some(mut cur_child_idx) = node.child_index {
-                loop {
-                    let cur_child_idx_usize = u32::from(cur_child_idx) as usize;
-                    draw_hierarchy_node(ui, graph, cur_child_idx_usize, selected_node_idx);
-                    cur_child_idx = match graph[cur_child_idx_usize].sibling_index {
-                        Some(x) => x,
-                        None => break
+            if node_idx == 0 || tree_node_token.is_some() {
+                // Draw child nodes recursively
+                if let Some(mut cur_child_idx) = node.child_index {
+                    loop {
+                        let cur_child_idx_usize = u32::from(cur_child_idx) as usize;
+                        self.draw_hierarchy_node(ui, cur_child_idx_usize);
+                        cur_child_idx = match self.graph[cur_child_idx_usize].sibling_index {
+                            Some(x) => x,
+                            None => break
+                        }
                     }
                 }
             }
+            if let Some(t) = tree_node_token { t.pop(); }
         }
-        if let Some(t) = tree_node_token { t.pop(); }
-    }
+    }    
 }
