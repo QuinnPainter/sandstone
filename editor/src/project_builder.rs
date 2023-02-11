b9d579e7b1cdc60cf0822d630cfe71af37a363c1
Quinn Painter <quinn@geekymonkey.com>
2023-02-11 22:00:08 +0000

    Build user projects and create ROM with ironds-romtool

--- editor/src/project_builder.rs
+++ editor/src/project_builder.rs
@@ -1,4 +1,6 @@
 use std::io::Write;
+use std::process::Command;
+use std::path::Path;
 use quote::quote;
 use crate::project_data::ProjectData;
 
@@ -13,35 +15,45 @@ pub fn build(project_data: &mut ProjectData) {
     // Create build folder if it doesn't exist
     let build_path = project_data.path.join("build");
     std::fs::create_dir_all(&build_path).unwrap();
+    let arm9_path = build_path.join("arm9_runtime");
+    let arm7_path = build_path.join("arm7_runtime");
 
     let arm9_code = quote! {
         #![no_std]
         #![no_main]
         extern crate alloc;
+        use core::num::NonZeroU32;
+        use alloc::boxed::Box;
+        use dsengine_user_code as user_code;
 
-        fn script_factory(id: NonZeroU32) -> Box<dyn Script> {
+        fn script_factory(id: NonZeroU32) -> Box<dyn dsengine::Script> {
             match u32::from(id) {
-                1 => Box::new(Obj1::default()),
-                2 => Box::new(Obj2::default()),
-                _ => panic!("Invalid component ID: {}", id)
+                1 => Box::new(user_code::Obj1::default()),
+                2 => Box::new(user_code::Obj2::default()),
+                _ => panic!("Invalid script ID: {}", id)
             }
         }
+
+        #[no_mangle]
+        extern "C" fn main() -> ! {
+            dsengine::hierarchy::init_script_factory(script_factory);
+            dsengine::hierarchy::init_prefab_data(include_bytes!("../../prefab_data.bin"));
+            dsengine::main_loop();
+        }
     };
-    create_runtime_crate(true, &build_path, &arm9_code.to_string());
+    create_runtime_crate(true, &arm9_path, &arm9_code.to_string());
 
     let arm7_code = quote! {
         #![no_std]
         #![no_main]
         extern crate alloc;
 
-        use dsengine_arm7;
-
         #[no_mangle]
         extern "C" fn main() -> ! {
             dsengine_arm7::main_loop();
         }
     };
-    create_runtime_crate(false, &build_path, &arm7_code.to_string());
+    create_runtime_crate(false, &arm7_path, &arm7_code.to_string());
     /*let mut saved_graph = dsengine_common::SavedNodeGraph {nodes: Vec::new()};
     saved_graph.nodes.push(dsengine_common::SavedNode {
         child_index: None,
@@ -73,23 +85,48 @@ pub fn build(project_data: &mut ProjectData) {
     let serialised_graphs = dsengine_common::serialize_prefabs(&dsengine_common::SavedPrefabs(project_data.export_saved_graph()));
     let mut graph_file = std::fs::File::create(build_path.join("graph_data.bin")).unwrap();
     graph_file.write_all(&serialised_graphs).unwrap();
+
+    let rom_path = build_path.join(project_data.name.clone() + ".nds");
+    match build_rom(&rom_path, &arm9_path, &arm7_path, false) {
+        Ok(_) => (),
+        Err(msg) => println!("{msg}")
+    }
 }
 
-fn create_runtime_crate(arm9: bool, path: &std::path::Path, code: &str) {
-    let (runtime_path, cargo_toml, cargo_config);
+fn build_rom(rom_path: &Path, arm9_path: &Path, arm7_path: &Path, release: bool) -> Result<(), String> {
+    build_runtime_crate(arm9_path, release)?;
+    build_runtime_crate(arm7_path, release)?;
+    ironds_romtool::build_rom(rom_path, arm9_path, arm7_path)?;
+    Ok(())
+}
+
+fn build_runtime_crate(path: &Path, release: bool) -> Result<(), String> {
+    let profile = if release { "release" } else { "dev" };
+    let cargo_output = Command::new("cargo")
+        .arg("build")
+        .args(["--profile", profile])
+        .current_dir(path)
+        .output().unwrap();
+    if cargo_output.status.success() {
+        Ok(())
+    } else {
+        Err(String::from_utf8_lossy(&cargo_output.stderr).to_string())
+    }
+}
+
+fn create_runtime_crate(arm9: bool, path: &Path, code: &str) {
+    let (cargo_toml, cargo_config);
     if arm9 {
-        runtime_path = path.join("arm9_runtime");
         cargo_toml = ARM9_CARGO;
         cargo_config = ARM9_CARGO_CONFIG;
     } else {
-        runtime_path = path.join("arm7_runtime");
         cargo_toml = ARM7_CARGO;
         cargo_config = ARM7_CARGO_CONFIG;
     }
     // Regenerate crate if it doesn't exist
-    if !runtime_path.join("Cargo.toml").exists() {
+    if !path.join("Cargo.toml").exists() {
         // Even if the cargo toml is missing, folder may still exist. in that case, delete it so we get a clean slate
-        match std::fs::remove_dir_all(&runtime_path) {
+        match std::fs::remove_dir_all(&path) {
             Ok(_) => (),
             // totally fine if the folder was not found, we were trying to delete it anyway!
             Err(err) if err.kind() == std::io::ErrorKind::NotFound => (),
@@ -97,14 +134,14 @@ fn create_runtime_crate(arm9: bool, path: &std::path::Path, code: &str) {
         }
 
         // Create directories
-        std::fs::create_dir_all(runtime_path.join(".cargo")).unwrap();
-        std::fs::create_dir_all(runtime_path.join("src")).unwrap();
+        std::fs::create_dir_all(path.join(".cargo")).unwrap();
+        std::fs::create_dir_all(path.join("src")).unwrap();
 
         // Create files
-        std::fs::write(runtime_path.join("Cargo.toml"), cargo_toml).unwrap();
-        std::fs::write(runtime_path.join(".cargo/config.toml"), cargo_config).unwrap();
-        std::fs::write(runtime_path.join("rust-toolchain.toml"), RUST_TOOLCHAIN).unwrap();
+        std::fs::write(path.join("Cargo.toml"), cargo_toml).unwrap();
+        std::fs::write(path.join(".cargo/config.toml"), cargo_config).unwrap();
+        std::fs::write(path.join("rust-toolchain.toml"), RUST_TOOLCHAIN).unwrap();
     }
-    std::fs::write(runtime_path.join("src/main.rs"), code).unwrap();
+    std::fs::write(path.join("src/main.rs"), code).unwrap();
 }
  
